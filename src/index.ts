9fd662781c85eb64a5e7da4d8e20acbe19228bb8
lihbr <lihbr@users.noreply.github.com>
2024-10-24 18:50:50 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 import type { Camera2Plugin } from './definitions';
 
 const Camera2 = registerPlugin<Camera2Plugin>('Camera2', {
-  web: () => import('./web').then(m => new m.Camera2Web()),
+  web: () => import('./web').then((m) => new m.Camera2Web()),
 });
 
 export * from './definitions';
--- src/web.ts
+++ src/web.ts
@@ -2,40 +2,9 @@ import { WebPlugin } from '@capacitor/core';
 
 import type { Camera2Plugin } from './definitions';
 
-const noop = (name: string) => async () => {
-  throw new Error(`Method ${name} is not implemented for web platform`);
-};
-
 export class Camera2Web extends WebPlugin implements Camera2Plugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
   }
-
-  start = noop('start');
-  stop = noop('stop');
-  capture = noop('capture');
-
-  setViewFinderSize = noop('setViewFinderSize');
-
-  getShutterSpeedRange = noop('getShutterSpeedRange');
-  setShutterSpeed = noop('setShutterSpeed');
-
-  getApertureRange = noop('getApertureRange');
-  setAperture = noop('setAperture');
-
-  getIsoRange = noop('getIsoRange');
-  setIso = noop('setIso');
-
-  getExposureCompensationInfo = noop('getExposureCompensationInfo');
-  setExposureCompensation = noop('setExposureCompensation');
-
-  openPIP = noop('openPIP');
-  closePIP = noop('closePIP');
-  
-  setPIPSize = noop('setPIPSize');
-  setPIPPosition = noop('setPIPPosition');
-
-  pictureToThumbnail = noop('pictureToThumbnail');
-  getExifData = noop('getExifData');
 }
